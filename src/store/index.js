c57053040310ab660eb01a660201ba3918050107
kdk1026 <kdk1026@gmail.com>
2024-05-31 12:59:27 +0900

    vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
 import { createStore } from "vuex";
 import numberStore from "./numberStore";
+import createPersistedState from "vuex-persistedstate";
 
 const store = createStore({
     modules: {
         number: numberStore
-    }
+    },
+    plugins: [
+        createPersistedState({
+            key: 'root',
+            storage: window.sessionStorage, // 생략 시, localStorage
+            paths: ['number']
+        })
+    ]
 });
 
 export default store;
